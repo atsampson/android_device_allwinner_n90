0e4d60c4be73e5ee1a7945efb144e2611cc49fb4
Adam Sampson <ats@offog.org>
2013-09-09 16:32:51 +0100

    Initial commit for device N90 by Sanei

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := SEDPRETTYNAME
+PRODUCT_RELEASE_NAME := N90
 
 # Boot animation
 TARGET_BOOTANIMATION_NAME := horizontal-1024x768
@@ -8,14 +8,14 @@ TARGET_BOOTANIMATION_NAME := horizontal-1024x768
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/allwinner/SEDNAME/full_SEDNAME.mk)
+$(call inherit-product, device/allwinner/n90/full_n90.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := SEDNAME
-PRODUCT_NAME := cm_SEDNAME
-PRODUCT_BRAND := SEDBRANDING
-PRODUCT_MODEL := SEDPRETTYNAME
+PRODUCT_DEVICE := n90
+PRODUCT_NAME := cm_n90
+PRODUCT_BRAND := Sanei
+PRODUCT_MODEL := N90
 PRODUCT_MANUFACTURER := Allwinner
 
 #Set build fingerprint / ID / Product Name etc
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SEDNAME BUILD_FINGERPRINT="allwinner/SEDNAME/SEDNAME:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="SEDNAME-user 4.0.4 IMM76I 330937 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=n90 BUILD_FINGERPRINT="allwinner/n90/n90:4.0.4/IMM76I/330937:user/release-keys" PRIVATE_BUILD_DESC="n90-user 4.0.4 IMM76I 330937 release-keys"
--- device.mk
+++ device.mk
@@ -12,16 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This file includes all definitions that apply only to SEDNAME devices
+# This file includes all definitions that apply only to n90 devices
 #
 # Anything that is generic to all allwinner products should go in the common directory
 #
 # Everything in this directory will become public
 
 $(call inherit-product, device/allwinner/common/device.mk)
-$(call inherit-product-if-exists, vendor/allwinner/SEDNAME/SEDNAME-vendor.mk)
+$(call inherit-product-if-exists, vendor/allwinner/n90/n90-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/allwinner/SEDNAME/overlay
+DEVICE_PACKAGE_OVERLAYS += device/allwinner/n90/overlay
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
@@ -30,11 +30,11 @@ PRODUCT_COPY_FILES += \
 #
 
 PRODUCT_COPY_FILES += \
-	device/allwinner/SEDNAME/init.sun4i.modules.rc:root/init.sun4i.modules.rc \
-	device/allwinner/SEDNAME/config/camera.cfg:system/etc/camera.cfg \
-	device/allwinner/SEDNAME/config/media_profiles.xml:system/etc/media_profiles.xml \
-	device/allwinner/SEDNAME/config/axp20-supplyer.kl:system/usr/keylayout/axp20-supplyer.kl \
-	device/allwinner/SEDNAME/config/sun4i-keyboard.kl:system/usr/keylayout/sun4i-keyboard.kl \
-	device/allwinner/SEDNAME/config/Goodix-TS-board-3.idc:system/usr/idc/Goodix-TS-board-3.idc \
-	device/allwinner/SEDNAME/config/devicespecific.sh:recovery/root/sbin/devicespecific.sh \
+	device/allwinner/n90/init.sun4i.modules.rc:root/init.sun4i.modules.rc \
+	device/allwinner/n90/config/camera.cfg:system/etc/camera.cfg \
+	device/allwinner/n90/config/media_profiles.xml:system/etc/media_profiles.xml \
+	device/allwinner/n90/config/axp20-supplyer.kl:system/usr/keylayout/axp20-supplyer.kl \
+	device/allwinner/n90/config/sun4i-keyboard.kl:system/usr/keylayout/sun4i-keyboard.kl \
+	device/allwinner/n90/config/Goodix-TS-board-3.idc:system/usr/idc/Goodix-TS-board-3.idc \
+	device/allwinner/n90/config/devicespecific.sh:recovery/root/sbin/devicespecific.sh \
 #
